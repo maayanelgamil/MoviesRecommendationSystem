730ac93646dba335a88372b3e15c33d270c2e221
levybeny <ySsdsp9j>
2017-05-06 13:25:27 +0300

    with function

--- Recommendation/Recommendation/DBconnection.cs
+++ Recommendation/Recommendation/DBconnection.cs
@@ -8,5 +8,13 @@ namespace Recommendation
 {
     class DBconnection
     {
+        const string DIRECTORY_PATH = "";
+
+        /// <summary>
+        /// sets the movie vector to the given list
+        /// </summary>
+        /// <param name="movieID">movie id</param>
+        /// <param name="movieVector">the given list to change</param>
+        public void getMovieVector(int movieID, ref List<UserRank> movieVector);
     }
 }
