dc66008741cf73567a257326f80435b4681871fc
kulikr <kulikr@post.bgu.ac.il>
2017-05-06 13:17:40 +0300

    create classes commit - kulik

---
+++ Recommendation/Recommendation/Ranker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recommendation
+{
+    /// <summary>
+    /// The class is used to rank the different movies saves for each movie the movies to recommend
+    /// </summary>
+    public class Ranker
+    {
+        int numOfMovies;
+        public Ranker(int _numOfMovies)
+        {
+            numOfMovies = _numOfMovies;
+        }
+
+        private List<String> getRecommendedMovies(List<UserRank> movieUsersRank)
+        {
+
+            return null;
+        }
+    }
+}
